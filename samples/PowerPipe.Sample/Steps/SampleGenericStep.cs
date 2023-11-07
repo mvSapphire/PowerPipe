b8ba38159f4958369f2ac17f54e36303abf28721
Max Vorchakov <m.vorchakov97@gmail.com>
2023-11-07 18:26:30 +0100

    Added method to register open generic type with specific lifetime (#43)

---
+++ samples/PowerPipe.Sample/Steps/SampleGenericStep.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PowerPipe.Interfaces;
+
+namespace PowerPipe.Sample.Steps;
+
+public class SampleGenericStep<T>: IPipelineStep<SamplePipelineContext>
+{
+    public IPipelineStep<SamplePipelineContext> NextStep { get; set; }
+
+    public async Task ExecuteAsync(SamplePipelineContext context, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"{nameof(SampleGenericStep<T>)} Executed");
+        await NextStep.ExecuteAsync(context, cancellationToken);
+    }
+}
