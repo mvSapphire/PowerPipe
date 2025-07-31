9faad220249a25203d3f11df37abb9b5cbf93bf8
Max Vorchakov <maksym.vorchakov@gr8.tech>
2025-07-31 12:17:11 +0200

    net9

--- tests/PowerPipe.UnitTests/PipelineDependencyInjectionTests.cs
+++ tests/PowerPipe.UnitTests/PipelineDependencyInjectionTests.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
+using AwesomeAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using PowerPipe.Builder;
 using PowerPipe.Exceptions;
